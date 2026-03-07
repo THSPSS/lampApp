088c116f9bfa27d1bc0c8e057419ce4e1d3c86ca
THSPSS <sunyeongbang98@gmail.com>
2026-03-07 21:17:19 +0900

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('your word is a lamp for my feet and a light on my path'),
+    );
+  }
+}
