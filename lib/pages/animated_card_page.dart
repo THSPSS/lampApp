7997aba02f9a2681a485f60e4875729a009711cb
THSPSS <sunyeongbang98@gmail.com>
2026-04-16 19:36:35 +0900

    update with rotate x card

--- lib/pages/animated_card_page.dart
+++ lib/pages/animated_card_page.dart
@@ -1,35 +1,81 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:lamp/global_variable.dart';
 
 class AnimatedCardPage extends StatefulWidget {
   @override
   AnimatedCardPageState createState() => AnimatedCardPageState();
 }
 
-class AnimatedCardPageState extends State<AnimatedCardPage> {
-  var alignment = Alignment.center;
+class AnimatedCardPageState extends State<AnimatedCardPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> moveDown;
+  late Animation<double> rotate;
+  late Animation<double> moveUp;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 10),
+    );
+
+    moveUp = Tween<double>(begin: 0, end: 150).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.0, 0.3, curve: Curves.easeOut),
+      ),
+    );
+
+    rotate = Tween<double>(begin: 0, end: 2 * pi).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.0, 0.3, curve: Curves.easeIn),
+      ),
+    );
+
+    moveDown = Tween<double>(begin: 150, end: 0).animate(
+      CurvedAnimation(
+        parent: controller,
+        curve: Interval(0.7, 1.0, curve: Curves.easeIn),
+      ),
+    );
+  }
+
+  void startAnimation() {
+    controller.forward(from: 0);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Animated Align with card"),
+        title: Text("Custom move with card"),
       ),
       body: GestureDetector(
-        onVerticalDragStart: (details) {
-          print("on vertical drag start ${details}");
-          setState(() {
-            alignment = alignment == Alignment.center
-                ? Alignment.topRight
-                : Alignment.center;
-          });
-        },
-        child: AnimatedAlign(
-          alignment: alignment,
-          duration: Duration(milliseconds: 100),
-          child: Container(
-            width: 300,
-            height: 150,
-            color: Colors.amberAccent,
+        onTap: startAnimation,
+        child: AnimatedBuilder(
+          animation: controller,
+          builder: (context, child) {
+            return Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()
+                ..setEntry(1, 3, 0.001)
+                ..rotateX(rotate.value), //perspective
+              child: child,
+            );
+          },
+          child: Center(
+            child: Container(
+              width: 300,
+              height: 150,
+              color: Colors.amberAccent,
+              child: Text(frontScripture[0]['content'].toString()),
+            ),
           ),
         ),
       ),
